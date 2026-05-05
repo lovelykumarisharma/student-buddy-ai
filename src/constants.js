be839245f4a6a7f7c2445a3909053c7c7290b3d5
LOVELY KUMARI <lk150303@gmail.com>
2026-05-05 19:56:56 +0530

    Delete src/constants.js

--- src/constants.js
+++
@@ -1,3 +0,0 @@
-// export const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=AIzaSyC-ReBGSQKm4uZNun99obfL-vg8wntU1UM`
-
-export const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyA1Qg_U6PuHsnkjGXmwKxos17RJjbLkJG4`
